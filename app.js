02ed3bec18ac9c31d3aad8bf8911f2eec4defbf6
JesminNancy <jesminnancy@hotmail.com>
2022-11-20 21:15:20 +0600

    Directives v-text & v-html

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ new Vue({
     data:{
         src:"https://picsum.photos/200",
         alt:"this is natural background",
-        link:"https://www.youtube.com/"
+        link:"https://www.youtube.com/",
+        myText:"Hello World",
+        myHtml:'<strong>Hello Jhonny</strong>'
     }
    
 });

c96e78808e06094f85d133c962f3b1aa78beef92
JesminNancy <jesminnancy@hotmail.com>
2022-11-22 01:21:42 +0600

    Style in Component

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -22,4 +22,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  font-family: "Roboto", sans-serif;
+}
+</style>
--- myapp/src/Components/TheFooter.vue
+++ myapp/src/Components/TheFooter.vue
@@ -1,3 +1,9 @@
 <template>
   <div>Footer</div>
 </template>
+<style scoped>
+div {
+  color: darkorange;
+  font-size: 40px;
+}
+</style>

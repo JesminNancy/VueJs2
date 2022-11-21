573b3f632d0f8917382c1ae4498c9d49ba17a521
JesminNancy <jesminnancy@hotmail.com>
2022-11-21 23:00:31 +0600

    New Project Creating With Vue Cli

---
+++ myapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Radifa",
+      age: 8,
+    };
+  },
+};
+</script>
+
+<style></style>

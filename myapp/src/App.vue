c1916392daa7648bd78635e4f113b66ffbba1a57
JesminNancy <jesminnancy@hotmail.com>
2022-11-22 00:46:50 +0600

    Create a Component & Locally Import

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
+    <compHeader />
+    <compFooter />
     <p>{{ name }}</p>
     <p>{{ age }}</p>
   </div>
 </template>
 
 <script>
+import compHeader from "./Components/TheHeader.vue";
+import compFooter from "./Components/TheFooter.vue";
 export default {
   data() {
     return {
       name: "Radifa",
       age: 8,
     };
+  },
+  components: {
+    compHeader,
+    compFooter,
   },
 };
 </script>

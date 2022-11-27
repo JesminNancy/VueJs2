a7f7eb1f380fb726ecf01845a6462d0686eee175
JesminNancy <jesminnancy@hotmail.com>
2022-11-27 19:01:04 +0600

    Use Slots

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
-    <compHeader :firstName="fName" />
-    <compFooter :lastName="lName" />
+    <compListHelper>
+      <ul slot="listItem">
+        <li v-for="li in list" :key="li">
+          {{ li }}
+        </li>
+      </ul>
+      <p slot="para">This is paragrap</p>
+      <p :slot="loader">See You Again</p>
+    </compListHelper>
   </div>
 </template>
 
 <script>
-import compHeader from "./Components/TheHeader.vue";
-import compFooter from "./Components/TheFooter.vue";
+import compListHelper from "./Components/ListHelper.vue";
 export default {
   data() {
     return {
-      fName: "Jhony",
-      lName: "Doe",
+      list: ["html", "css", "javaScript", "php", "laravel"],
+      loader: "",
     };
   },
   components: {
-    compHeader,
-    compFooter,
+    compListHelper,
+  },
+  created() {
+    setTimeout(() => {
+      this.loader = "loading";
+    }, 3000);
   },
 };
 </script>
@@ -25,5 +35,8 @@ export default {
 <style>
 body {
   font-family: "Roboto", sans-serif;
+}
+p {
+  color: blue;
 }
 </style>
---
+++ myapp/src/Components/ListHelper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot name="listItem"></slot>
+    I am a list helper
+    <slot name="para"></slot>
+    <slot name="loading">Loading...</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+ul li {
+  color: brown;
+}
+</style>

6fe55387dc26b02ab0bcced124ee6270a06578ce
JesminNancy <jesminnancy@hotmail.com>
2022-11-28 17:08:38 +0600

    Dynamic Component

---
+++ myapp/src/Components/TheContact.vue
@@ -0,0 +1,187 @@
+<template>
+  <div>
+    <form>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <h1>From Handeling</h1>
+          <hr />
+          <div class="form-group">
+            <label for="email">E-mail</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              v-model="userData.email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model.lazy="userData.password"
+            />
+            <p>{{ userData.password }}</p>
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              class="form-control"
+              v-model.number="userData.age"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
+        >
+          <label for="message">Message</label><br />
+
+          <textarea
+            id="message"
+            rows="5"
+            class="form-control"
+            v-model="message"
+          >
+          </textarea>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <div class="form-group">
+            <label for="sendmail">
+              <input
+                type="checkbox"
+                id="sendmail"
+                value="SendMail"
+                v-model="sendmail"
+              />
+              Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input
+                type="checkbox"
+                id="sendInfomail"
+                value="SendInfoMail"
+                v-model="sendmail"
+              />
+              Send Infomail
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
+        >
+          <label for="male">
+            <input type="radio" id="male" value="Male" v-model="gender" /> Male
+          </label>
+          <label for="female">
+            <input type="radio" id="female" value="Female" v-model="gender" />
+            Female
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group"
+        >
+          <label for="priority">Priority</label>
+          <select id="priority" class="form-control" v-model="priority">
+            <option v-for="priority in priorities" :key="priority">
+              {{ priority }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <hr />
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <button class="btn btn-primary" @click.prevent="submitted">
+            Submit!
+          </button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row" v-if="isSubmitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>E-mail: {{ userData.email }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Age: {{ userData.age }}</p>
+            <p style="white-space: pre-line">Message: {{ message }}</p>
+            <p>
+              <strong>Send Mail: {{ sendmail }}</strong>
+            </p>
+
+            <!-- <ul>
+              <li v-for="mail in sendmail" :key="mail">
+                {{ mail }}
+              </li>
+            </ul> -->
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{ priority }}</p>
+            <p>Switched:</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: {
+        email: "",
+        password: "",
+        age: 0,
+      },
+      message: "",
+      sendmail: [],
+      gender: "",
+      priority: "Low",
+      priorities: ["High", "Medium", "Low"],
+      isSubmitted: false,
+    };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    },
+  },
+
+  destroyed() {
+    console.log("destroyed");
+  },
+  activated() {
+    console.log("Activated");
+  },
+  deactivated() {
+    console.log("Deactivated");
+  },
+};
+</script>
+
+<style>
+ul li {
+  list-style: none;
+}
+</style>

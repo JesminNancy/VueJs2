9c312f274a27d2386c074d47bedd4ee35dd2cdb1
JesminNancy <jesminnancy@hotmail.com>
2022-11-28 20:56:00 +0600

    Transition

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="container">
-    <div v-something.red.big="'Hello World'"></div>
-    <div v-something.blue.small="'Hello World'"></div>
+    <transition name="appear">
+      <div class="p-3 mb-3 bg-success text-white" v-if="display">Hello</div>
+    </transition>
+
+    <button @click="display = !display" class="btn btn-primary">Toggle</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      display: false,
+    };
+  },
+};
+</script>
+<style>
+.appear-enter {
+  opacity: 0;
+}
+.appear-enter-active {
+  transition: opacity 3s;
+}
+.appear-leave {
+}
+.appear-leave-active {
+  opacity: 0;
+  transition: opacity 3s;
+}
+</style>
